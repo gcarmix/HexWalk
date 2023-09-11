2e5632ce8629e3b22a3ab1ce46a4dfef3e8ce936
Carmix <carmixdev@gmail.com>
2023-09-11 08:37:26 +0200

    for windows release

--- binwalk_windows/binw.py
+++ binwalk_windows/binw.py
@@ -1,5 +1,7 @@
-import binwalk
 import sys
+import os
+sys.path.append(os.path.dirname('binwalk/src/binwalk'))
+import binwalk
 if len(sys.argv) == 2:
     binwalk.scan('--signature', sys.argv[1])
 elif len(sys.argv) == 3 and sys.argv[1] == '-e':
