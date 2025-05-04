6f703551af561025f44687ee177fb70638248fbd
Carmix <carmixdev@gmail.com>
2025-05-04 16:33:33 +0200

    mod for compatibility with binwalk 3

--- hexwalk/binanalysisdialog.cpp
+++ hexwalk/binanalysisdialog.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2025 Carmix <carmixdev@gmail.com>
+ *
+ * This file is part of HexWalk.
+ *
+ * HexWalk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HexWalk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "binanalysisdialog.h"
 #include "ui_binanalysisdialog.h"
 #include <QFile>
@@ -282,7 +300,8 @@ void binanalysisdialog::on_extractAllBtn_clicked()
 #else
     QDir d = QFileInfo(curFile).absoluteDir();
     QString curDir=d.absolutePath();
-    params << "-e" << curFile;
+    curDir=curDir+"/extracted";
+    params << "-e" << curFile << "-C"<<curDir;
     binwalkProcess->start("binwalk",params);
 #endif
 
