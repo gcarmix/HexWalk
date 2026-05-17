977fe7710974d6dbc6aa8d1f92dfc7c476c88692
gcarmix <carmixdev@gmail.com>
2026-05-17 20:38:34 +0200

    removed old windows binwalk dependency, added debian package for 2.0.0

--- binwalk_windows/binw.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname('binwalk/src/binwalk'))
-import binwalk
-if len(sys.argv) == 2:
-    binwalk.scan('--signature', sys.argv[1])
-elif len(sys.argv) == 3 and sys.argv[1] == '-e':
-    binwalk.scan('--signature','--extract', sys.argv[2])
